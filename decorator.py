6aaf766a6bd1a74a71ce5ff5e49c86c0b4927ee0
donihakerboy <77894892+donihakerboy@users.noreply.github.com>
2023-12-23 06:05:16 +0600

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,10 +1,13 @@
 from tkinter import messagebox
+
+
 class Decorator:
     def __init__(self, observer):
         self._observer = observer
 
     def update(self, message) -> str:
         messagebox.showwarning(message=f"{message}")
+
 
 class ShowMessageBoxDecorator(Decorator):
     def update(self, message) -> str:
